3ca5f1d08d45a71813e5422be9a6c237adfb41fc
Matt Bush <bush.matthewjames@gmail.com>
2019-02-27 18:07:23 +0000

    Crossfilter csv data and test in browser

--- docs/static/js/charts.js
+++ docs/static/js/charts.js
@@ -1,8 +1,13 @@
 queue()
-    .defer(d3.csv, 'docs/static/data/boroughs-data.csv')
-    .await(makeGraphs);
+    .defer(d3.csv, 'docs/static/data/boroughs-data.csv') // load data first
+    .await(makeGraphs); // load data into makeGraphs function when complete or throw error if an error 
 
+// ==== main makeGraphs function to render all charts
 function makeGraphs(error, csv){
 
-    
+     // crossfilter csv data
+     var cf = crossfilter(londonCsv);
+
 }
+
+// ==== total population number display
