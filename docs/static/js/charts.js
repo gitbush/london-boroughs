8b1786d474ab0ab7ad702b4c7fe2828ccdd64caf
Matt Bush <bush.matthewjames@gmail.com>
2019-02-28 16:52:51 +0000

    Add population number at #population
    Observe all filters with groupAll on crossfilter.
    Renames makeGraphs() to makeCharts()

--- docs/static/js/charts.js
+++ docs/static/js/charts.js
@@ -1,13 +1,34 @@
 queue()
     .defer(d3.csv, 'docs/static/data/boroughs-data.csv') // load data first
-    .await(makeGraphs); // load data into makeGraphs function when complete or throw error if an error 
+    .await(makeCharts); // load data into makeCharts function when complete or throw error if an error 
 
-// ==== main makeGraphs function to render all charts
-function makeGraphs(error, csv){
+// ==== main makeCharts function to render all charts
+function makeCharts(error, csv){
 
-     // crossfilter csv data
-     var cf = crossfilter(londonCsv);
+    // format strings into numbers
+    csv.forEach(function (d){
+        d.GLA_Population_Estimate = +d.GLA_Population_Estimate;
+    });
 
+    // crossfilter csv data
+    var cf = crossfilter(csv);
+
+    // all charts
+    populationNd(cf);
+
+    dc.renderAll();
 }
 
 // ==== total population number display
+function populationNd(cf) {
+    // group on total population with groupAll on crossfilter to observe all filter when applied
+    var popGroup = cf.groupAll().reduceSum(function(d){return d.GLA_Population_Estimate});
+    // create number display at #population
+    var popNd = dc.numberDisplay("#population");
+    // dc number display
+    popNd
+        .group(popGroup)
+        .valueAccessor(function(d){
+            return d;
+        });
+}
