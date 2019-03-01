89fe632c794fdacffb62dbd52d5a795901d341c3
Matt Bush <bush.matthewjames@gmail.com>
2019-03-01 14:01:24 +0000

    Move en-GB locale into its own d3_locale.js

--- docs/static/js/charts.js
+++ docs/static/js/charts.js
@@ -4,22 +4,7 @@ queue()
 
 // ==== main makeCharts function to render all charts
 function makeCharts(error, csv){
-    // ==== format d3.locale for the UK
-    // en-GB locale format
-    var GBLocale = {
-        "decimal": ".",
-        "thousands": ",",
-        "grouping": [3],
-        "currency": ["£", ""],
-        "dateTime": "%a %b %e %X %Y",
-        "date": "%d/%m/%Y",
-        "time": "%H:%M:%S",
-        "periods": ["AM", "PM"],
-        "days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-        "shortDays": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-        "months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        "shortMonths": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    }
+    
     // format d3.locale  
     var GB = d3.locale(GBLocale);
 
